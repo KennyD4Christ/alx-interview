b6d63cae8379a25c7d50f112d918cd02a95968bc
KennyD4Christ <kennyd4christ@gmail.com>
2024-09-04 09:33:22 +0100

    0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,37 +2,25 @@
 
 const request = require('request');
 
-// Get the Movie ID from the first positional argument
-const movieID = process.argv[2];
+const movieId = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
 
-// Construct the URL to fetch the movie details
-const url = `https://swapi-api.alx-tools.com/api/films/${movieID}/`;
-
-// Make the request to the API
 request(url, (error, response, body) => {
   if (error) {
     console.error(error);
     return;
   }
-
-  // Parse the response body as JSON
-  const movieData = JSON.parse(body);
-
-  // Extract the character URLs from the movie data
-  const characters = movieData.characters;
-
-  // Fetch and print each character's name in the order they appear in the list
-  characters.forEach((characterURL) => {
-    request(characterURL, (error, response, body) => {
-      if (error) {
-        console.error(error);
+  const film = JSON.parse(body);
+  const characters = film.characters;
+  
+  characters.forEach((characterUrl) => {
+    request(characterUrl, (err, res, charBody) => {
+      if (err) {
+        console.error(err);
         return;
       }
-
-      // Parse the character data and print the name
-      const characterData = JSON.parse(body);
-      console.log(characterData.name);
+      const character = JSON.parse(charBody);
+      console.log(character.name);
     });
   });
 });
-
